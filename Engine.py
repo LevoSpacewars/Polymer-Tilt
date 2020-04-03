9d754398408a06de3d8de0c8d829dce7ec27f2a7
LevoSpacewars <awpatapoff@gmail.com>
2020-04-02 20:44:55 -0700

    testing

--- Engine.py
+++ Engine.py
@@ -1,36 +1,32 @@
 import Simulations
 import math
-# parameters = Simulations.PolymerSimulationParameters()
-# dt = 0.0000001
-# runl = 100000000
-# vAverage = 0.05
-# vs = 1000000
-# A=1
-# print("\n\n\n\n\n")
-# parameters.setLength(100)
-# parameters.setNumberChains(8)
-# parameters.setPairRadius(.3)
-# parameters.setPairPotentialStrength(10e4)
-# parameters.setPairRadiusEqualibrium(.3)
-# parameters.setPairMaximumRadius(0.03)
-# parameters.setForcePull(20)
-# parameters.setPeriodicAmplitude(A)
-# parameters.setGamma(0.5)
-# parameters.setKBT(1)
-# parameters.setTimeStep(dt)
-# parameters.setProbePeriod(vs)
-# parameters.setRunLength(runl)
-#
-# sim = Simulations.PolymerSimulation()
-#
-# sim.init(parameter=parameters,initializer='--mode=gpu')
-#
-# filelocation = sim.run(forceRange=[0,5,10])
-# print(filelocation)
+parameters = Simulations.PolymerSimulationParameters()
+dt = 0.0000001
+runl = 10000000
+vAverage = 0.05
+vs = 100000
+A=1
+print("\n\n\n\n\n")
+parameters.setLength(100)
+parameters.setNumberChains(8)
+parameters.setPairRadius(.3)
+parameters.setPairPotentialStrength(10e4)
+parameters.setPairRadiusEqualibrium(.3)
+parameters.setPairMaximumRadius(0.03)
+parameters.setForcePull(20)
+parameters.setPeriodicAmplitude(A)
+parameters.setGamma(0.5)
+parameters.setKBT(1)
+parameters.setTimeStep(dt)
+parameters.setProbePeriod(vs)
+parameters.setRunLength(runl)
 
-#renderer = Simulations.DataVisualizer(basedirectory="Simulation_2020-04-01_22-01-19/",interval=0.5)
-#renderer.init()
-filelocation="Simulation_2020-04-01_22-01-19/"
-a = Simulations.GlobalDataAnalyzer(filelocation)
+sim = Simulations.PolymerSimulation(parameter=parameters)
 
-a.plotTiltbyForce(20,1)
+filelocation = sim.run(forceRange=[0,1,1])
+print(filelocation)
+
+renderer = Simulations.DataVisualizer(basedirectory=filelocation,interval=0)
+# a = Simulations.GlobalDataAnalyzer(filelocation)
+#
+# a.plotTiltbyForce(20,1)
