e7c9ad6e92f26bec44d1162569c2c7011152637c
LevoSpacewars <awpatapoff@gmail.com>
2021-09-12 14:26:40 -0700

    more stuff

---
+++ submit_scattered_Ljobs.py.py
@@ -0,0 +1,50 @@
+#here we are going to submit parallel jobs with group ID
+#first give name to all the inputs
+#then assign sysargs to a dictionary
+
+
+args = ["length", "kbt", "amplitude", "tension", "npolymers", "runtime", "samplerate", "sheer_start","sheer_end","DF","ID","disorder_level","ModalDisorder","ncomm"]
+dic = {}
+import sys
+import os
+import random
+for i in range(1,len(sys.argv)):
+    dic.update({args[i-1]: sys.argv[i], (i-1):sys.argv[i]})
+
+
+# Now calculate sheer array
+sheers = []
+tstart = float(dic["sheer_start"])
+tend = float(dic["sheer_end"])
+dif = tend - tstart
+conv = dif / float(dic["DF"])
+rseed = random.randint(1,1000000)
+for i in range(int(dic["DF"])):
+    sheers.append(conv * i + tstart)
+
+print("probing sheers: " + str(sheers))
+
+#now let's get all the values in order
+argsouput = []
+for j in range(int(dic["DF"])):
+    sargs = ""
+    for i in range(7):
+        sargs += " " + dic[i]
+    sargs += " " + str(sheers[j]) + " " + str(tend) + " " + dic["ID"] + " " + str(rseed) + " " + str(dic['disorder_level'] + " " + str(dic['ModalDisorder'])) + " " + str(dic['ncomm'])
+
+    argsouput.append(sargs)
+print(argsouput)
+#here we need to create the individual batch files that are going to call the Engine_fast_runs.py [args]
+filen = "Engine_fast_runs.py"
+settings = "#!/bin/bash \n#SBATCH --job-name=Poltmer_Run_scattered     ### Job Name \n#SBATCH --partition=longgpu	  ### Quality of Service (like a queue in PBS) \n#SBATCH --time=14-00:00:00     ### Wall clock time limit in Days-HH:MM:SS \n#SBATCH --nodes=1             ### Node count required for the job \n#SBATCH --ntasks-per-node=1   ### Nuber of tasks to be launched per Node \n#SBATCH --gres=gpu:1          ### General REServation of gpu:number of gpus \n#SBATCH --account=softmatter    ### Account used for job submission \n"
+cmd = "python3 /home/apatapof/softmatter/Polymer-Tilt/" + filen
+for i in range(int(dic["DF"])):
+    filename = dic["ID"] + "_" + str(sheers[i]) + ".batch"
+    print("creating: " + filename)
+    cmda = cmd + argsouput[i] + " $SLURM_JOB_GPUS"
+    f = open(filename, "w")
+    f.write(settings)
+    f.write(cmda)
+    f.close()
+    print("submitting job for: " + filename)
+    os.system("sbatch " + filename)
